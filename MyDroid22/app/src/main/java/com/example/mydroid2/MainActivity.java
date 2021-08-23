a99a652791947226d1f26170c17d5349392f705b
Zaeem Tarrar <zaeemtarrar3@gmail.com>
2021-08-24 02:26:27 +0500

    android 0.0.1

--- MyDroid22/app/src/main/java/com/example/mydroid2/MainActivity.java
+++ MyDroid22/app/src/main/java/com/example/mydroid2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mydroid2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -9,27 +10,48 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.mydroid2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
     
-    private TextView hobbyBar;
-    private Button hobbyButton;
-    private TextView hobbyResult;
+//    private TextView hobbyBar;
+//    private Button hobbyButton;
+//    private TextView hobbyResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        hobbyBar = findViewById(R.id.hobby_input);
-        hobbyButton = findViewById(R.id.hobby_button);
-        hobbyResult = findViewById(R.id.hobby_result);
+//        setContentView(R.layout.activity_main);
+
+//        hobbyBar = findViewById(R.id.hobby_input);
+//        hobbyButton = findViewById(R.id.hobby_button);
+//        hobbyResult = findViewById(R.id.hobby_result);
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+//        binding.hobbyButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                addHobby(view);
+//            }
+//        });
+
+        binding.hobbyButton.setOnClickListener(this::addHobby);
     }
 
     public void addHobby(View view) {
-        String data = hobbyBar.getText().toString();
-        hobbyBar.setText("");
-        hobbyResult.setText("Hobbies: "+data.toString());
-        hobbyResult.setVisibility(View.VISIBLE);
+//        String data = hobbyBar.getText().toString();
+//        hobbyBar.setText("");
+//        hobbyResult.setText("Hobbies: "+data.toString());
+//        hobbyResult.setVisibility(View.VISIBLE);
+
+        String data = binding.hobbyInput.getText().toString();
+        binding.hobbyInput.setText("");
+        binding.hobbyResult.setText("Hobbies: "+data.toString());
+        binding.invalidateAll();
+        binding.hobbyResult.setVisibility(View.VISIBLE);
 
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
--- README.md
+++ README.md
@@ -71,3 +71,92 @@ inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
 		- Contraint Layout
 		- Frame Layout
 			- Scroll View
+
+### View Binding
+
+```java
+buildFeatures {
+    dataBinding true
+}
+```
+
+**Note:** Sync after adding the Code
+
+```java
+android {
+    compileSdk 31
+
+    defaultConfig {
+        applicationId "com.example.mydroid2"
+        minSdk 22
+        targetSdk 31
+        versionCode 1
+        versionName "1.0"
+
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        dataBinding true
+    }
+}
+```
+
+Then, change the Activity Layout as Well
+
+```xml
+<layout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    tools:context=".MainActivity">
+    <androidx.appcompat.widget.LinearLayoutCompat
+        android:layout_width="match_parent"
+        android:background="@color/teal_700"
+        android:orientation="vertical"
+        android:layout_height="match_parent">
+    </androidx.appcompat.widget.LinearLayoutCompat>
+</layout>
+```
+
+Then, Use Binding inside the Activity
+
+```java
+private ActivityMainBinding binding;
+```
+
+```java
+binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+binding.hobbyButton.setOnClickListener(new View.OnClickListener(){
+    @Override
+    public void onClick(View view) {
+        addHobby(view);
+    }
+});
+```
+
+```java
+String data = binding.hobbyInput.getText().toString();
+binding.hobbyInput.setText("");
+binding.hobbyResult.setText("Hobbies: "+data.toString());
+binding.invalidateAll();
+binding.hobbyResult.setVisibility(View.VISIBLE);
+```
+
+Using Lambda
+
+```java
+binding.hobbyButton.setOnClickListener(this::addHobby);
+```
+
+Then, remove Event Listeners from the Activity XML File
